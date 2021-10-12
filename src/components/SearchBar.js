e3133ae35732c02d3fec42ace1cd16fcf40c6a87
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-12 23:01:59 +0300

    refactor: SearchBar.js revised.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    render() {
-        return (
-            <div>
-                Search Bar 
-            </div>
-        )
-    }
+ 
+  handleFormSubmit =(event) => {
+      event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleFormSubmit}>
+        <div className="form-row mb-5">
+          <div className="col-12">
+            <input
+              onChange={this.props.searchMovie}
+              type="text"
+              className="form-control"
+              placeholder="Search a movie"
+            />
+          </div>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default SearchBar;
