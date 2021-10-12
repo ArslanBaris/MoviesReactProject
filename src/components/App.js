b35bba9314a5ea81415778c6d6497d1d503c43f8
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-12 20:08:45 +0300

    refactor: App.js revised.

--- src/components/App.js
+++ src/components/App.js
@@ -36,6 +36,15 @@ class App extends React.Component {
 
     }
 
+    deleteMovie = (movie) => {
+      const newMovieList = this.state.movies.filter(
+        m => m.id !== movie.id
+      )
+      this.setState({
+        movies: newMovieList
+      })
+    }
+
   render() {
     return (
         <div className='container'>
@@ -45,7 +54,8 @@ class App extends React.Component {
                 </div>
             </div>
             <MovieList 
-            movies ={this.state.movies} />
+            movies ={this.state.movies}
+            deleteMovieProp={this.deleteMovie} />
         </div>
         );
   }
