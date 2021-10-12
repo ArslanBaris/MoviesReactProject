769fc79ecd554b377b0c8951b20937b283fe5881
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-12 13:04:43 +0300

    feat: SearchBar.js added.

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <div>
+                Search Bar 
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
