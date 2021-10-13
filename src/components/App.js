8c092ffa310613fce6c866dfd99b147b071246de
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-13 16:36:29 +0300

    Fake Service added.

--- src/components/App.js
+++ src/components/App.js
@@ -4,40 +4,17 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 
-    state = { movies : [
-        {
-          id: 1,
-          name: "The Flash",
-          rating: 8.3,
-          overview:
-            "This is a wider card with supporting text below as a natural lead-in to additional content.",
-          imageURL:
-            "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg",
-        },
-      
-        {
-          id: 2,
-          name: "Interstellar",
-          rating: 6.8,
-          overview:
-            "This is a wider card with supporting text below as a natural lead-in to additional content.",
-          imageURL:
-            "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-        },
-      
-        {
-          id: 3,
-          name: "Arrow",
-          rating: 7.9,
-          overview:
-            "This is a wider card with supporting text below as a natural lead-in to additional content.",
-          imageURL:
-            "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg",
-        },
-    ],
+    state = {
+      movies : [],
+      searchQuery :""
 
-    searchQuery :""
+    }
 
+    async componentDidMount(){
+      const baseURL = "http://localhost:3002/movies";
+      const response = await fetch(baseURL);
+      const data = await response.json();
+      this.setState({movies : data})
     }
 
     deleteMovie = (movie) => {
