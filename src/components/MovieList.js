8706e62930e250032d3b2f0fe8e6446879adf80d
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-15 23:18:21 +0300

    refactor: MovieList.js revised.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
+
 const MovieList = (props) => {
   
     return (
@@ -10,12 +12,12 @@ const MovieList = (props) => {
           <div className="col-lg-4" key={movie.id}>
             <div className="card mb-4 shadow-sm">
               <img
-                src={movie.imageURL}
+                src={`${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`}
                 className="card-img-top"
                 alt="Sample Movie"
               />
               <div className="card-body">
-                <h5 className="card-title">{movie.name}</h5>
+                <h5 className="card-title">{movie.title}</h5>
                 <p className="card-text">{movie.overview}</p>
                 <div className="d-flex justify-content-between align-items-center">
                   <button
@@ -24,7 +26,7 @@ const MovieList = (props) => {
                     onClick={(event)=> props.deleteMovieProp(movie)}
                   >Delete</button>
                   <h2>
-                    <span className="badge badge-info">{movie.rating}</span>
+                    <span className="badge badge-info">{movie.vote_average}</span>
                   </h2>
                 </div>
               </div>
