8d6d1d63cfe4ff5f3bfc0659bbfc4afc25ef4cb7
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-16 18:44:11 +0300

    refactor: SearchBar.js revised.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 class SearchBar extends React.Component {
   handleFormSubmit = (event) => {
@@ -17,13 +18,14 @@ class SearchBar extends React.Component {
             />
           </div>
           <div className="col-2">
-            <button
+            <Link
+              to="/add"
               type="button"
               className="btn btn-md btn-danger"
               style={{ float: "right" }}
             >
               Add Movie
-            </button>
+            </Link>
           </div>
         </div>
       </form>
