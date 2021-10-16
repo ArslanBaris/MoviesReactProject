6c981f73a2be53366b8012407795ae96e3e96c32
Barış Arslan <75984280+ArslanBaris@users.noreply.github.com>
2021-10-16 18:44:24 +0300

    refactor: MovieList.js revised.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -5,9 +5,9 @@ const MovieList = (props) => {
     return (
       <div className="row">
 
-        {props.movies.map((movie) => (
+        {props.movies.map((movie,i) => (
 
-          <div className="col-lg-3" key={movie.id}>
+          <div className="col-lg-3" key={i}>
             <div className="card mb-4 shadow-sm">
               <img
                 src={movie.imageURL}
